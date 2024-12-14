4d5b0db8e5e3857f3c19e7bb0962c5ac5b73376f
AGulev <me@agulev.com>
2024-12-14 18:32:39 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Gimme Defold value
+
+A tiny extension that contains functions to convert Defold types into different values.
+---
---
+++ extension-gimme/src/gimme.cpp
@@ -0,0 +1,50 @@
+// Extension lib defines
+#define LIB_NAME "GimmeExtension"
+#define MODULE_NAME "gimme"
+
+#include <dmsdk/sdk.h>
+#if defined(DM_PLATFORM_VENDOR)
+#include <dmsdk/dlib/hash_vendor.h>
+#elif defined(__linux__) && !defined(__ANDROID__)
+#define DM_HASH_LONG_FMT "%lu"
+#else
+#define DM_HASH_LONG_FMT "%llu"
+#endif
+
+static int Gimme_HashVal(lua_State* L)
+{
+    dmhash_t hash = dmScript::CheckHash(L, 1);
+    char buffer[64];
+    dmSnPrintf(buffer, sizeof(buffer), DM_HASH_LONG_FMT, hash);
+    lua_pushstring(L, buffer);
+    return 1;
+}
+
+#undef DM_HASH_LONG_FMT
+
+static const luaL_reg Module_methods[] =
+{
+    {"hash_val", Gimme_HashVal},
+    {0, 0}
+};
+
+static void LuaInit(lua_State* L)
+{
+    int top = lua_gettop(L);
+
+    // Register lua names
+    luaL_register(L, MODULE_NAME, Module_methods);
+
+    lua_pop(L, 1);
+    assert(top == lua_gettop(L));
+}
+
+dmExtension::Result InitializeGimmeExtension(dmExtension::Params* params)
+{
+    // Init Lua
+    LuaInit(params->m_L);
+    dmLogDebug("Registered '%s' Extension\n", MODULE_NAME);
+    return dmExtension::RESULT_OK;
+}
+
+DM_DECLARE_EXTENSION(GimmeExtension, LIB_NAME, 0, 0, InitializeGimmeExtension, 0, 0, 0)
